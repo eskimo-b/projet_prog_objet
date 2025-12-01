0d8ddbbe733f4af3634003b50b642017880b3240
CHAHMA Nael <Nael.Chahma@bordeaux-inp.fr>
2025-12-01 15:17:09 +0100

    ajout ensembleBorne

--- com/schottenTotten/controller/EnsembleBorne.java
+++ com/schottenTotten/controller/EnsembleBorne.java
@@ -1,8 +1,21 @@
 public class EnsembleBorne{
     private Borne[] bornes;
+    private int nb_bornes; 
 
-    public EnsembleBorne(Borne[] b){
-        this.bornes = b;
+    public EnsembleBorne(Borne[] bornes){
+        this.bornes = bornes;
+        this.nb_bornes = bornes.length;
     }
-    
+
+    public int getNombreBornes(bornes, joueur){
+        int acc = 0; 
+        for (int i = 0; i < bornes.length; i++){
+            if (bornes[i].proprietaire == joueur){
+                acc++;
+            }
+        }
+        return acc;
+    }
+
+    public 
 }
---
+++ com/schottenTotten/model/CarteClan.java
@@ -0,0 +1,12 @@
+package com.schottenTotten.model;
+
+public class CarteClan extends Carte {
+ 
+    public CarteClan() {
+        super();//appel constructeur vide de carte
+    }
+
+    public CarteClan(int valeur, Couleur couleur) {
+        super(valeur, couleur);
+    }
+}
